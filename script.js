b61a5e6a287efa1ee383174b686a3dd02f6d99b7
Shanil <Shanilanees@gmail.com>
2022-10-01 00:36:10 +0500

    Html and css done.

---
+++ script.js
@@ -0,0 +1,23 @@
+const subtract = document.querySelector(".subtract");
+const addition = document.querySelector(".addition");
+const reset = document.querySelector(".reset");
+const count = document.querySelector(".counter");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener('click', (e)=>{
+    if (e.target.classList.contains('addition')){
+
+    }
+})
+
+
+
+addition.addEventListener("click", () => {
+  count.innerHTML++;
+});
+subtract.addEventListener("click", () => {
+  count.innerHTML--;
+});
+reset.addEventListener("click", () => {
+  count.innerHTML = 0;
+});
