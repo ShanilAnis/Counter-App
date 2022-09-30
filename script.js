c55065fa176d0f0b296b4633d95cfe724cf5b2b7
Shanil <Shanilanees@gmail.com>
2022-10-01 01:11:37 +0500

    event delegation done.

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-const subtract = document.querySelector(".subtract");
-const addition = document.querySelector(".add");
-const reset = document.querySelector(".reset");
+// const subtract = document.querySelector(".subtract");
+// const addition = document.querySelector(".add");
+// const reset = document.querySelector(".reset");
 const count = document.querySelector(".counter");
 const buttons = document.querySelector(".buttons");
 
-// buttons.addEventListener('click', (e)=>{
-//     if (e.target.classList.contains('addition')){
+buttons.addEventListener('click', (e)=>{
+    if (e.target.classList.contains('add')){
+         count.innerHTML++
+    }
+    else if (e.target.classList.contains('subtract')){
+         count.innerHTML--
+    }
+    else if (e.target.classList.contains('reset')){
+         count.innerHTML = 0
+    }
+})
 
-//     }
-// })
 
-
-addition.addEventListener("click", () => {
-  count.innerHTML++;
-});
-subtract.addEventListener("click", () => {
-  count.innerHTML--;
-});
-reset.addEventListener("click", () => {
-  count.innerHTML = 0;
-});
+// addition.addEventListener("click", () => {
+//   count.innerHTML++;
+// // });
+// subtract.addEventListener("click", () => {
+//   count.innerHTML--;
+// });
+// reset.addEventListener("click", () => {
+//   count.innerHTML = 0;
+// });
